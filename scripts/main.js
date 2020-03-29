0b4b018907b2d1f4f74fe2dafd9b83251215df5b
Irina Kramer <irakramer@gmail.com>
2020-03-29 00:27:55 -0700

    Applied fix for from PR in original repo

--- scripts/main.js
+++ scripts/main.js
@@ -35,13 +35,13 @@ function StickyNotesApp() {
   }
 
   // Listen for updates to notes from other windows.
-  window.addEventListener('storage', function(e) {
+  window.addEventListener('storage', function (e) {
     this.displayNote(e.key, e.newValue);
   }.bind(this));
 }
 
 // Saves a new sticky note on localStorage.
-StickyNotesApp.prototype.saveNote = function() {
+StickyNotesApp.prototype.saveNote = function () {
   if (this.noteMessageInput.value) {
     var key = Date.now().toString();
     localStorage.setItem(key, this.noteMessageInput.value);
@@ -52,14 +52,14 @@ StickyNotesApp.prototype.saveNote = function() {
 };
 
 // Resets the given MaterialTextField.
-StickyNotesApp.resetMaterialTextfield = function(element) {
+StickyNotesApp.resetMaterialTextfield = function (element) {
   element.value = '';
   element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
   element.blur();
 };
 
 // Creates/updates/deletes a note in the UI.
-StickyNotesApp.prototype.displayNote = function(key, message) {
+StickyNotesApp.prototype.displayNote = function (key, message) {
   var note = document.getElementById(key);
   // If no element with the given key exists we create a new note.
   if (!note) {
@@ -75,7 +75,7 @@ StickyNotesApp.prototype.displayNote = function(key, message) {
 };
 
 // Enables or disables the submit button depending on the values of the input field.
-StickyNotesApp.prototype.toggleButton = function() {
+StickyNotesApp.prototype.toggleButton = function () {
   if (this.noteMessageInput.value) {
     this.addNoteButton.removeAttribute('disabled');
   } else {
@@ -84,19 +84,24 @@ StickyNotesApp.prototype.toggleButton = function() {
 };
 
 // On load start the app.
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   new StickyNotesApp();
 });
 
 // A Sticky Note custom element that extends HTMLElement.
-var StickyNote = Object.create(HTMLElement.prototype);
+
+var StickyNote = function () {
+  return Reflect.construct(HTMLElement, [], new.target);
+}
+
+StickyNote.prototype = Object.create(HTMLElement.prototype);
 
 // Initial content of the element.
 StickyNote.TEMPLATE =
   '<div class="message"></div>' +
   '<div class="date"></div>' +
   '<button class="delete mdl-button mdl-js-button mdl-js-ripple-effect">' +
-    'Delete' +
+  'Delete' +
   '</button>';
 
 // StickyNote elements top level style classes.
@@ -105,11 +110,11 @@ StickyNote.CLASSES = ['mdl-cell--4-col-desktop', 'mdl-card__supporting-text', 'm
 
 // List of shortened month names.
 StickyNote.MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov',
-                     'Dec'];
+  'Dec'];
 
 // Fires when an instance of the element is created.
-StickyNote.createdCallback = function() {
-  StickyNote.CLASSES.forEach(function(klass) {
+StickyNote.prototype.connectedCallback = function () {
+  StickyNote.CLASSES.forEach(function (klass) {
     this.classList.add(klass);
   }.bind(this));
   this.innerHTML = StickyNote.TEMPLATE;
@@ -120,7 +125,7 @@ StickyNote.createdCallback = function() {
 };
 
 // Fires when an attribute of the element is added/deleted/modified.
-StickyNote.attributeChangedCallback = function(attributeName) {
+StickyNote.prototype.attributeChangedCallback = function (attributeName) {
   // We display/update the created date message if the id changes.
   if (attributeName == 'id') {
     if (this.id) {
@@ -134,18 +139,16 @@ StickyNote.attributeChangedCallback = function(attributeName) {
 };
 
 // Sets the message of the note.
-StickyNote.setMessage = function(message) {
+StickyNote.prototype.setMessage = function (message) {
   this.messageElement.textContent = message;
   // Replace all line breaks by <br>.
   this.messageElement.innerHTML = this.messageElement.innerHTML.replace(/\n/g, '<br>');
 };
 
 // Deletes the note by removing the element from the DOM and the data from localStorage.
-StickyNote.deleteNote = function() {
+StickyNote.prototype.deleteNote = function () {
   localStorage.removeItem(this.id);
   this.parentNode.removeChild(this);
 };
 
-customElements.define('sticky-note', {
-  prototype: StickyNote
-});
+customElements.define('sticky-note', StickyNote);
